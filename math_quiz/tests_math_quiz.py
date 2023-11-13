9bbbf0c708ad88c13364f6afa3abca29c54eb4cc
venky0705 <vemalavenkatesh12345.com>
2023-11-13 20:22:56 +0100

    commiting after creating the unitests

--- math_quiz/tests_math_quiz.py
+++ math_quiz/tests_math_quiz.py
@@ -1,30 +1,40 @@
 import unittest
-from math_quiz import function_A, function_B, function_C
+from math_quiz import returnRandom, operation_choice, result
 
 
 class TestMathGame(unittest.TestCase):
 
-    def test_function_A(self):
+    def test_returnRandom(self):
         # Test if random numbers generated are within the specified range
         min_val = 1
         max_val = 10
         for _ in range(1000):  # Test a large number of random values
-            rand_num = function_A(min_val, max_val)
+            rand_num = returnRandom(min_val, max_val)
             self.assertTrue(min_val <= rand_num <= max_val)
 
-    def test_function_B(self):
-        # TODO
-        pass
+    def test_operation_choice(self):
+        # Test if operation_choice returns one of the valid operators
+        self.assertTrue(operation_choice() == '+' or operation_choice() == '-' or operation_choice() == '*')
 
-    def test_function_C(self):
-            test_cases = [
-                (5, 2, '+', '5 + 2', 7),
-                ''' TODO add more test cases here '''
-            ]
+    def test_result(self):
+        # Test all the three operators
+        test_cases = [
+            (5, 2, '+', '5 + 2', 7),
+            (1, 2, '-', '1 - 2', -1),
+            (5, 5, '*', '5 * 5', 25),
+        ]
+        # looping over the input test cases for all three operators
+        for number1, number2, operator, expected_problem, expected_answer in test_cases:
+            if operator == '+':
+                (self.assertEquals(result(number1, number2, operator)[1], number1 + number2) and self.assertEquals(
+                    result(number1, number2, operator)[0], expected_answer))
+            elif operator == '-':
+                (self.assertEquals(result(number1, number2, operator)[1], number1 - number2) and self.assertEquals(
+                    result(number1, number2, operator)[0], expected_answer))
+            else:
+                (self.assertEquals(result(number1, number2, operator)[1], number1 * number2) and self.assertEquals(
+                    result(number1, number2, operator)[0], expected_answer))
 
-            for num1, num2, operator, expected_problem, expected_answer in test_cases:
-                # TODO
-                pass
 
 if __name__ == "__main__":
     unittest.main()
